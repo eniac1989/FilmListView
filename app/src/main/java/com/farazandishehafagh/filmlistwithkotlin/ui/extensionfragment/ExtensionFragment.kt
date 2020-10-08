17de12863a4a1295374f3bdef0eb1a06de1a85b9
paniz <alipourpaniz@gmail.com>
2020-10-08 21:05:13 +0330

    990717

--- app/src/main/java/com/farazandishehafagh/filmlistwithkotlin/ui/extensionfragment/ExtensionFragment.kt
+++ app/src/main/java/com/farazandishehafagh/filmlistwithkotlin/ui/extensionfragment/ExtensionFragment.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import com.farazandishehafagh.filmlistwithkotlin.BaseApplication
 import com.farazandishehafagh.filmlistwithkotlin.R
 import com.farazandishehafagh.filmlistwithkotlin.utils.Utils
 import kotlinx.android.synthetic.main.fragment_extension.*
@@ -45,6 +44,7 @@ class ExtensionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        val txtName = findViewById<EditText>(R.id.btnFormatAmount)
         btnFormatAmount.setOnClickListener {
             val amount = txtAmount.text.toString().toLong()
             val formatAmount = amount.formatAmount()
